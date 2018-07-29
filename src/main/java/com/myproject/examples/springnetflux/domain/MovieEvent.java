2c2904fa5236957800ba78bfb4f0c0d13a058597
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-07-29 12:58:50 +0530

    Adding domains

---
+++ src/main/java/com/myproject/examples/springnetflux/domain/MovieEvent.java
@@ -0,0 +1,15 @@
+package com.myproject.examples.springnetflux.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+public class MovieEvent {
+
+    private String movieId;
+    private Date date;
+
+}
