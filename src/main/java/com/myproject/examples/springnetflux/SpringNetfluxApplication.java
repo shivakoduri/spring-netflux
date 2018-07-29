a5ac60ffd016659d799f9ca92c0343afd3085d91
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-07-29 12:46:52 +0530

    Initial commit for Spring-Netflux example

---
+++ src/main/java/com/myproject/examples/springnetflux/SpringNetfluxApplication.java
@@ -0,0 +1,12 @@
+package com.myproject.examples.springnetflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNetfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNetfluxApplication.class, args);
+    }
+}
